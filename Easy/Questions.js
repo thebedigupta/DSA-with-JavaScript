50d2a5bafdad290657d0688abfa9c8813d50dca8
Bedi Gupta <bedigupta76@gmail.com>
2025-04-15 23:51:42 +0530

    Practice Few Questions

--- Easy/Questions.js
+++ Easy/Questions.js
@@ -1,4 +1,79 @@
 // Problem 1 : Create an array with 5 Students names, after that create a function which takes 2 parameters (allStudents & studentName ) iterate over all students and find that specific user 'studentName'.
 
-let allStudents = ['Bedi','Ayush','Ravi','Richa','Nandini'];
+// let allStudents = ['Bedi','Ayush','Ravi','Richa','Nandini'];
 
+// class MyArray {
+//     constructor(){
+//         this.length = 0;
+//         this.data = {};
+//     }
+//     push(item){
+//         this.data[this.length] = item;
+//         this.length++;
+//         return this.length;
+//     }
+
+//     get(num){
+//         return this.data[num];
+//     }
+    
+//     pop(){
+//         const lastItem = this.data[this.length -1];
+//         delete this.data[this.length - 1];
+//         this.length--;
+//         return lastItem;
+//     }
+//     bedi(){
+//         const firstEle = delete this.data[0];
+
+//         for(let i = 0;i < this.length;i++ ){
+//             this.data[i] = this.data[i+1];
+//         }
+
+//         delete this.data[this.length - 1];
+//         this.length--;
+//         return firstEle
+//     }
+ 
+// }
+// const myNewArray = new MyArray();
+// myNewArray.push('apple');
+// myNewArray.push('Orange');
+// myNewArray.push('Banana');
+// myNewArray.push('Mango');
+// myNewArray.push('Pomegranate');
+
+// console.log(myNewArray);
+
+// // console.log(myNewArray.bedi());
+// // console.log(myNewArray.deleteByIndex(2));
+
+// console.log(myNewArray);
+
+// const ooLee = 'Hello'
+// console.log(ooLee.sort());
+
+// const leeBo = 'bddb';
+// const splitMethod = leeBo.split('');
+// console.log(splitMethod);
+// const reverseSplit = splitMethod.reverse();
+// console.log(reverseSplit);
+// const joinArray = reverseSplit.join('');
+
+// if(joinArray === leeBo){
+//     console.log(`${leeBo} is a Palindromes` );
+// }
+// else{
+//     console.log(`${leeBo} is not a Palindromes` );
+// }
+    
+
+// const nob = 12345;
+// const arrayNum = nob.split('');
+// console.log(arrayNum);
+// const reverseNum = arrayNum.reverse();
+// console.log(reverseNum)
+// const num = reverseNum.join('');
+// console.log(num)
+
+ 
