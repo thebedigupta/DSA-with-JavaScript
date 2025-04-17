9a41391766fc0c00686bd46f049287356574a9e2
Bedi Gupta <bedigupta76@gmail.com>
2025-04-17 23:57:55 +0530

    Trying to do twosum array question in JS

--- Easy/Questions.js
+++ Easy/Questions.js
@@ -1,79 +1,12 @@
-// Problem 1 : Create an array with 5 Students names, after that create a function which takes 2 parameters (allStudents & studentName ) iterate over all students and find that specific user 'studentName'.
+const twosum = function(nums,target){
+    for (let value of nums)
+        console.log(value) ;
+    for (i = 2; i <= nums.length;i++){
+        console.log(i);
+    }
 
-// let allStudents = ['Bedi','Ayush','Ravi','Richa','Nandini'];
+}
 
-// class MyArray {
-//     constructor(){
-//         this.length = 0;
-//         this.data = {};
-//     }
-//     push(item){
-//         this.data[this.length] = item;
-//         this.length++;
-//         return this.length;
-//     }
+const nums = [1,2,3,4,5,6,7,8,9];
 
-//     get(num){
-//         return this.data[num];
-//     }
-    
-//     pop(){
-//         const lastItem = this.data[this.length -1];
-//         delete this.data[this.length - 1];
-//         this.length--;
-//         return lastItem;
-//     }
-//     bedi(){
-//         const firstEle = delete this.data[0];
-
-//         for(let i = 0;i < this.length;i++ ){
-//             this.data[i] = this.data[i+1];
-//         }
-
-//         delete this.data[this.length - 1];
-//         this.length--;
-//         return firstEle
-//     }
- 
-// }
-// const myNewArray = new MyArray();
-// myNewArray.push('apple');
-// myNewArray.push('Orange');
-// myNewArray.push('Banana');
-// myNewArray.push('Mango');
-// myNewArray.push('Pomegranate');
-
-// console.log(myNewArray);
-
-// // console.log(myNewArray.bedi());
-// // console.log(myNewArray.deleteByIndex(2));
-
-// console.log(myNewArray);
-
-// const ooLee = 'Hello'
-// console.log(ooLee.sort());
-
-// const leeBo = 'bddb';
-// const splitMethod = leeBo.split('');
-// console.log(splitMethod);
-// const reverseSplit = splitMethod.reverse();
-// console.log(reverseSplit);
-// const joinArray = reverseSplit.join('');
-
-// if(joinArray === leeBo){
-//     console.log(`${leeBo} is a Palindromes` );
-// }
-// else{
-//     console.log(`${leeBo} is not a Palindromes` );
-// }
-    
-
-// const nob = 12345;
-// const arrayNum = nob.split('');
-// console.log(arrayNum);
-// const reverseNum = arrayNum.reverse();
-// console.log(reverseNum)
-// const num = reverseNum.join('');
-// console.log(num)
-
- 
+console.log(twosum(nums));
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ __Example Below__
 
 __Example Below__
 
-![Screenshot from 2025-04-15 10-18-16](https://github.com/user-attachments/assets/7a383945-6000-4e34-8636-f095e02e879a)
+![Algorithm](../DSA-with-JavaScript/assests/Algorithm.png)
 
 
 __So in the above example I showed you what actually algorithm is and data structure__
@@ -38,3 +38,52 @@ __Main Motive To Learn DSA is you become Great Problem Solver__
 > Big O Notation helps us understand how long an algorithm will take to run or how much memory it will need as the amount of data it handles grows
 
 __When we combine time complexity and space complexity then we called Big O Notation. In Simple terms time complexity means how much time that algorithm would take to run and space complexity means how much space it was going to take to run this code__
+
+__Understand Big O Notation with Real Life Analogy__
+
+![Big O Analogy in Real Life](../DSA-with-JavaScript/assests/Big%20O%20Analogy.png)
+
+(i) __First Big O Notation__
+
+* __O(n)__ (Read As O of n)
+
+![Definition Of O of n](../DSA-with-JavaScript/assests/O%20of%20n.png)
+
+In Simple Words
+
+![O of n Analogy](../DSA-with-JavaScript/assests/O%20of%20n%20Real%20LIfe%20Analogy.png)
+
+Example
+
+![O of n Example](../DSA-with-JavaScript/assests/O%20of%20n%20example.png)
+
+* __Dropping the Constant's__
+
+  Why O(2n ) is also called O(n) ?
+
+![Real Life Ananlogy](../DSA-with-JavaScript/assests/Real%20life%20Example%20O%20of%202n.png)
+
+![Real Life Ananlogy Part 2](../DSA-with-JavaScript/assests/Real%20Life%20Example%20O%20of%202n%20(Part%202).png)
+
+* __O(1)__ (Read As O of one)
+
+![Definition of O of 1](../DSA-with-JavaScript/assests/Defintion%20O%20of%20One.png)
+
+Understand with Real Life Ananlogy
+
+![Example of O of 1](../DSA-with-JavaScript/assests/Real%20Life%20Example%20of%20O%20of%20One.png)
+
+Example in code
+
+![Example code of O of 1](../DSA-with-JavaScript/assests/Example%20code%20of%20O%20of%201.png)
+
+* __O(n^2)__ (O of n ^ 2)
+
+![Defintion of O of n power 2](../DSA-with-JavaScript/assests/Definition%20of%20O%20of%20n%20Power%202.png)
+
+Example
+
+[Example of O of n power 2](../DSA-with-JavaScript/assests/Example%20of%20O%20of%20n%20Power%202.png)
+
+Code Sample for brief Understanding
+
